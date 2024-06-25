87a710eac8e8f7c8d3d52ab9e91a75b4d1bb4875
AlishGautam-12 <alishgautam12@gmail.com>
2024-06-25 09:14:32 +0545

    Add Quiz component and related files

---
+++ Quize/src/Data/QuizData.jsx
@@ -0,0 +1,19 @@
+export const QuizData = [
+    {
+        question:"What is your name?",
+        options:["Alish","Bora","Namik","Rupesh"],
+        answer:3
+    },
+    {
+        question:"Who is vergin?",
+        options:["Alish","Bora","Namik","Rupesh"],
+        answer:3
+    },
+
+    {
+        question:"IS bora Gay?",
+        options:["Yes","No","option A","only option A"],
+        answer:1
+    },
+
+]
